a661973c983e5fae0d5a0a540969c28339e868b8
AlessandraGrenha <77298334+AlessandraGrenha@users.noreply.github.com>
2021-03-04 11:57:33 -0300

    Add files via upload

---
+++ Num_Multipos.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class Num_Multipos {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner (System.in);
+		
+		int n1;
+		int n2;
+		
+		System.out.print("Digite o primeiro valor inteiro: ");
+		n1 = input.nextInt();
+		
+		System.out.print("Digite o segundo valor inteiro: ");
+		n2 = input.nextInt();
+		
+		if ((n1 % n2) == 0 || (n2 % n1) == 0) {
+			System.out.printf("%d e %d são múltiplos entre si", n1 , n2);
+		}
+		else {
+			System.out.printf("%d e %d não são múltiplos entre si", n1 , n2);
+		}
+
+	}
+
+}
